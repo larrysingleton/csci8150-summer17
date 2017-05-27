f4d9b78e38864ba2e6a5d18b8a6a418617f22aa1
zwmcfarland@gmail.com <1midnight5115^>
2017-05-27 14:56:37 -0500

    First implementation of instruction cache.

--- src/data_structures/instruction_cache.c
+++ src/data_structures/instruction_cache.c
@@ -1,1 +1,16 @@
-// TODO: Implement a shared data structure that will act as the instruction cache for the CPU to read instructions from.
+#include <main.h>
+
+extern instruction* instructionCache[INS_CACHE_ROWS][INS_CACHE_CELLS];
+
+/* Macro used to decode the cache row from a program counter */
+#define row(programCounter)  (programCounter / INS_CACHE_CELLS)
+/* Macro used to decode the cache row cell from a program counter */
+#define cell(programCounter)  (programCounter % INS_CACHE_CELLS)
+
+instruction* fetch(int programCounter) {
+    return instructionCache[row(programCounter)][cell(programCounter)];
+}
+
+void loadCache(int programCounter, instruction* instructionPointer) {
+    instructionCache[row(programCounter)][cell(programCounter)] = instructionPointer;
+}
--- src/include/data_structures.h
+++ src/include/data_structures.h
@@ -1,8 +1,14 @@
-//
-// Created by cazam on 5/27/2017.
-//
 
 #ifndef CSCI8700_DATA_STRUCTURES_H_H
 #define CSCI8700_DATA_STRUCTURES_H_H
+
+
+// Each row is 64 bytes, we allocate 64 rows
+// for a total of 512 byte instruction cache
+#define INS_CACHE_ROWS 64 //64 rows in the instruction cache.
+#define INS_CACHE_CELLS 8 //8 Instructions per row.
+#define INS_SIZE_IN_BYTES 8
+
+typedef unsigned char instruction[INS_SIZE_IN_BYTES]; // An instruction is made up of 8 bytes
 
 #endif //CSCI8700_DATA_STRUCTURES_H_H
